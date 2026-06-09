199545cbeec1a8e46ee5312a4af9633d7bef2185
Артём Каримов <grandbo@list.ru>
2026-06-09 13:24:38 +0500

    Создал 3 рабочих файла

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+    let a = document.getElementById("answer");
+    let i = document.getElementById("inc");
+
+    let n = 0;
+
+    i.innerHTML = n;
+
+    function increment() {
+        //n++;
+        i.innerHTML++;
+        //console.log(n);
+    }
+
+    
+    function decrement() {
+        //n++;
+        i.innerHTML--;
+        //console.log(n);
+    }
